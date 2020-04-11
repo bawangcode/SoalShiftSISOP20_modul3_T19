7651a5dba09e440ac07044dc93262c939c4d33f0
Rinnabel <55181896+Rinnabel@users.noreply.github.com>
2020-04-11 15:52:57 +0800

    Update soal4c.c

--- soal4/soal4c.c
+++ soal4/soal4c.c
@@ -1,7 +1,31 @@
-#include <stdio.h>
-#include <string.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<string.h>
+#include<sys/wait.h>
+
+int main() {
+  int deskriptor[2];
+  pid_t pid;
+  pipe(deskriptor);
+
+  pid = fork();
+  if (pid == 0) {
+    dup2(deskriptor[1], 1);
+    close(deskriptor[0]);
+    close(deskriptor[1]);
+    char *argv[] = {"ls", NULL};
+    execv("/bin/ls", argv);
+  }
+  while(wait(NULL) > 0);
+
+  pid = fork();
+  if (pid == 0) {
+    dup2(deskriptor[0], 0);
+    close(deskriptor[0]);
+    close(deskriptor[1]);
+    char *argv[] = {"wc", "-l", NULL};
+    execv("/usr/bin/wc", argv);
+  }
+}
